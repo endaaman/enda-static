f918652fe401e659d935082ee0640bc4df1dda61
endaaman <endaaman@eis.hokudai.ac.jp>
2016-10-26 03:37:39 +0900

    chage root

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 var port = 3001
 var server = express()
 
-server.use(express.static('/var/uploaded/enda'))
+server.use(express.static('/var/www/uploads/enda/'))
 server.listen(port, function() {
   console.log(`Started stub server(port:${port})`)
 })
